3e9df2fb183d800f48320b505a2dabd4977dd2ec
luying <18280977332@163.com>
2023-08-20 09:39:50 +0800

    首页标语

--- src/api/banner.js
+++ src/api/banner.js
@@ -7,3 +7,12 @@ export function getBanner() {
     method: "get",
   })
 }
+
+// 设置首页标语
+export function setBanner(data) {
+  return request({
+    url: "/api/banner",
+    method: "post",
+    data,
+  })
+}
